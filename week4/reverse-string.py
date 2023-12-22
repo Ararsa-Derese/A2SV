7ba618c0775d8891370ec4beb66e99268e4b5bd3
Ararsa <116437655+Ararsa-Derese@users.noreply.github.com>
2023-12-22 19:25:15 +0300

    Added solution for Reverse String

--- week4/reverse-string.py
+++ week4/reverse-string.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        if len(s) < 1:
-             return
-        c = s.pop(0)
-        self.reverseString(s)
-        s.append(c)
-         
-
+        i,j=0,len(s)-1
+        while i<=j:
+            s[i],s[j]=s[j],s[i]
+            i+=1
+            j-=1
 
         
