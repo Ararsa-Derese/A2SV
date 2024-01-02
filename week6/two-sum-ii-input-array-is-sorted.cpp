ef22ecba31b3ba0943a838d38d3fd98229020dc8
Ararsa <116437655+Ararsa-Derese@users.noreply.github.com>
2024-01-02 18:42:24 +0300

    Added solution for Two Sum II - Input Array Is Sorted

--- week6/two-sum-ii-input-array-is-sorted.cpp
+++ week6/two-sum-ii-input-array-is-sorted.cpp
@@ -4,12 +4,7 @@ public:
         int i=0,j=numbers.size()-1;
         while(i<j){
             if(numbers[i]+numbers[j]==target){
-                int a=distance(numbers.begin(),find(numbers.begin(),numbers.end(),numbers[i]));
-                int b=distance(numbers.begin(),find(numbers.begin(),numbers.end(),numbers[j]));
-                if(a==b){
-                  return {a+1,b+2};  
-                }
-                return {a+1,b+1};
+                return {i+1,j+1};
                 
             }
             else if(numbers[i]+numbers[j]>target)
