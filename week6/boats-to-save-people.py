119dcababa18b3e0b136f7a6b5cda41aadaef839
Ararsa <116437655+Ararsa-Derese@users.noreply.github.com>
2024-01-04 19:16:18 +0300

    Added solution for Boats to Save People

--- week6/boats-to-save-people.py
+++ week6/boats-to-save-people.py
@@ -1,13 +1,16 @@
 class Solution:
     def numRescueBoats(self, people: List[int], limit: int) -> int:
-        i = 0
-        j = len(people) - 1
-        count = 0
         people.sort()
-        while i <= j:
+        sum=0
+        count=0
+        i=0
+        j=len(people)-1
+        while i<=j:
             if people[i] + people[j] <= limit:
-                  i+=1
-            j-=1
-            count+=1
-        
-        return count                    
+                j-=1
+                i+=1
+                count+=1
+            else:
+                j-=1
+                count+=1
+        return count
