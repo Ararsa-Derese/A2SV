b68ed1ce09e897363c1ca3d92deee0afaec81364
Ararsa <116437655+Ararsa-Derese@users.noreply.github.com>
2024-01-11 17:40:00 +0300

    Add solution for MinimumRecolorstoGetKConsecutiveBlackBlocks

---
+++ LeetCode/MinimumRecolorstoGetKConsecutiveBlackBlocks.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+            mini=k
+            if len(blocks)==k:
+                return blocks.count('W')
+            for i in range(len(blocks)-k+1):
+                if blocks[i:i+k].count('W')<mini:
+                    mini=blocks[i:i+k].count('W')
+            return mini
