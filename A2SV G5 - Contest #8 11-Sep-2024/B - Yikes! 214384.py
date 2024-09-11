c2600c64d258d8eb7eb6ffd72d5e976e692366fd
Ararsa <116437655+Ararsa-Derese@users.noreply.github.com>
2024-09-11 21:37:32 +0300

    Created 'B - Yikes!' by A2SV Hub

---
+++ A2SV G5 - Contest #8 11-Sep-2024/B - Yikes! 214384.py
@@ -0,0 +1,18 @@
+# Problem: B - Yikes! - https://codeforces.com/gym/511242/problem/B
+
+from bisect import bisect_left, bisect_right
+n = int(input())
+a = list(map(int, input().split()))
+m = int(input())
+b = list(map(int, input().split()))
+
+c = [1, a[0]] 
+x , y = 1, a[0]
+for i in range(n-1):
+    c.append(x+a[i])
+    c.append(y+a[i+1])
+    x = c[-2]
+    y = c[-1]
+for i in range(m):
+    print((bisect_right(c, b[i])+1)//2)
+    
