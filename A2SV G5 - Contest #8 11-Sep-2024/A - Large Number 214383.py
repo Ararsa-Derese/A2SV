272c53c4f71cc6685165baa9d42859257d8dbb37
Ararsa <116437655+Ararsa-Derese@users.noreply.github.com>
2024-09-11 21:37:22 +0300

    Created 'A - Large Number' by A2SV Hub

---
+++ A2SV G5 - Contest #8 11-Sep-2024/A - Large Number 214383.py
@@ -0,0 +1,14 @@
+# Problem: A - Large Number - https://codeforces.com/gym/511242/problem/A
+
+from bisect import bisect_left
+t = int(input())
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = input()
+    b=list(a)
+    for i in range(len(b)):
+        if k > int(b[i]):
+            print(''.join(b[:i])+str(k)+''.join(b[i:]))
+            break
+    else:
+        print(a+str(k))
